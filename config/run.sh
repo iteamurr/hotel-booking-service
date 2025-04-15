a71b646f0b2def71f68c78feaf69e7a802054597
iteamurr <iteamurr@gmail.com>
2025-04-15 20:19:17 +0300

    fix: Change project structure

--- config/run.sh
+++ config/run.sh
@@ -2,6 +2,6 @@
 
 export PYTHONPATH=$(pwd)
 
-cd src/database/ && poetry run alembic upgrade heads
+cd src/infrastructure/db/ && poetry run alembic upgrade heads
 
-cd ../.. && poetry run python -m src
+cd ../../.. && poetry run python -m src
